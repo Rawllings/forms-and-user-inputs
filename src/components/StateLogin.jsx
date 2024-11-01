d759e55e16e61148bf082cae375ef88d07b12aca
Rawllings Kiprop <rutojr01@gmail.com>
2024-11-01 15:23:30 +0300

    Done with validating forms on every keystroke via state but it is not cool. I does not give a user a chance

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -9,6 +9,9 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid =
+    enteredValues.email !== "" && !enteredValues.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -50,6 +53,9 @@ export default function Login() {
             onChange={(e) => handleChangeInput("email", e.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
